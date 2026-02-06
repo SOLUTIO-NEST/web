f760c0481ce6770fbd889b7589827d14197d2466
JangYeongHu <jyhoo040912@gmail.com>
2026-02-07 00:54:10 +0900

    fix: 머지하며 발생한 빌드 오류 수정

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from '@/context/AuthContext';
 
-import { AuthProvider } from '@/context/AuthContext';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
